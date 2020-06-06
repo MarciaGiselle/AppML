d3f8a21085db926945a0bfdaca7caaa203560b06
MarciaGiselle <marcitoledo@alumno.unlam.edu.ar>
2020-06-06 02:31:30 -0300

    Activity producto

--- AppMeli/app/src/main/java/com/example/appmeli/MainActivity.kt
+++ AppMeli/app/src/main/java/com/example/appmeli/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.appmeli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.searchButton)
+
+        //Intent representa la intención que tiene una app de realizar una tarea.
+        button.setOnClickListener{
+            val intent = Intent(this, ProductActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
